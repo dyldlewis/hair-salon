671667722a68ec828a385aa609a1bd547c06b863
Dylan Lewis <dyldlewis@gmail.com>
2017-07-14 10:03:29 -0700

    create and pass getAll and deleteAll tests

--- src/Client.php
+++ src/Client.php
@@ -26,13 +26,35 @@ class Client
     }
 
     function save()
-{
-      $executed = $GLOBALS['DB']->exec("INSERT INTO clients (name) VALUES ('{$this->getName()}');");
-      if ($executed) {
-            return true;
-      } else {
-            return false;
-      }
-}
+    {
+          $executed = $GLOBALS['DB']->exec("INSERT INTO clients (name) VALUES ('{$this->getName()}');");
+          if ($executed) {
+                return true;
+          } else {
+                return false;
+          }
+    }
+
+    static function getAll()
+    {
+        $returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients;");
+        $clients = array();
+        foreach($returned_clients as $client) {
+            $name = $client['name'];
+            $new_client = new Client($name);
+            array_push($clients, $new_client);
+        }
+    return $clients;
+    }
+
+    static function deleteAll()
+    {
+        $executed = $GLOBALS['DB']->exec("DELETE FROM clients;");
+        if ($executed) {
+           return true;
+        } else {
+           return false;
+        }
+    }
 }
 ?>
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -13,8 +13,12 @@
     $DB = new PDO($server, $username, $password);
 
 
-    class TaskTest extends PHPUnit_Framework_TestCase
+    class ClientTest extends PHPUnit_Framework_TestCase
     {
+        protected function tearDown()
+        {
+            Client::deleteAll();
+        }
 
         function test_save()
         {
@@ -28,5 +32,41 @@
             // Assert
             $this->assertTrue($executed, "Task not successfully saved to database");
         }
+
+        function testGetAll()
+        {
+            //Arrange
+            $name = "Cindy";
+            $name2 = "Lary";
+            $test_client = new Client($name);
+            $test_client->save();
+            $test_client2 = new Client($name2);
+            $test_client2->save();
+
+            //Act
+            $result = Client::getAll();
+
+            //Assert
+            $this->assertEquals([$test_client, $test_client2], $result);
+        }
+
+        function testDeleteAll()
+        {
+            //Arrange
+            $name = "Bill";
+            $name2 = "Jerry";
+            $test_client = new Client($name);
+            $test_client->save();
+            $test_client2 = new Client($name2);
+            $test_client2->save();
+
+            //Act
+            Client::deleteAll();
+
+            //Assert
+            $result = Client::getAll();
+            $this->assertEquals([], $result);
+        }
+
     }
 ?>
