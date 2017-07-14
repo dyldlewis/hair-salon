ecfe2777b9920c8f0b46eedf543edd16826b23cf
Dylan Lewis <dyldlewis@gmail.com>
2017-07-14 12:34:07 -0700

    create home route with corresponding twig teplate

--- app/app.php
+++ app/app.php
@@ -1,8 +1,9 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/MyClass.php";
+    require_once __DIR__."/../src/Client.php";
+    require_once __DIR__."/../src/Stylist.php";
 
-    $server = 'mysql:host=localhost:8889;dbname=to_do';
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -13,4 +14,14 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
+    $app->get("/", function() use ($app) {
+    return $app['twig']->render('index.html.twig');
+    });
+
+    // $app->get("/cuisines", function() use ($app) {
+    //     return $app['twig']->render('index.html.twig', array('cuisines' => Cuisines::getAll()));
+    // });
+
+
     return $app;
+?>
